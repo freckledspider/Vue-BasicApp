d2f948224a890103f78161f501ec68d08e9bb33f
bri <bcamesi@gmail.com>
2023-03-10 21:02:46 -0500

    create friend contact components with methods

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h2>{{ friends.name }}</h2>
+        <button @click="toggleDetails">Show Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friends.phone }}</li>
+            <li><strong>Email:</strong> {{ friends.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            detailsAreVisible: false,
+            friends: {
+                    id: 'manuel',
+                    name: "Manuel Lorenz",
+                    phone: '123 456 7890',
+                    email: 'manuel@localhost.com'
+                },
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible
+        }
+    }
+};
+</script>
