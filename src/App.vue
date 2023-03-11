6a0a02ca807150d566fb400527137d7f6c4a1a2a
bri <bcamesi@gmail.com>
2023-03-10 20:45:46 -0500

    create script with data and template

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<section>
+    <h2>My Friends</h2>
+    <ul>
+        <li></li>
+    </ul>
+</section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'manuel',
+                    name: "Manuel Lorenz",
+                    phone: '123 456 7890',
+                    email: 'manuel@localhost.com'
+                },
+                {
+                    id: 'julie',
+                    name: "Julie Jones",
+                    phone: '098 765 4321',
+                    email: 'julie@localhost.com'
+                }
+            ]
+        }
+    }
+};
+</script>
